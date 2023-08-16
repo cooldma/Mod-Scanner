03f97a0908284ca46eb4c25bcd43ef90ddf5cfe3
sixnoinegames <100712573+sixnoinegames@users.noreply.github.com>
2023-08-15 21:42:36 -0400

    The saviour we never we needed..

---
+++ src/main/java/net/scanner/util/OutputUtil.java
@@ -0,0 +1,16 @@
+package net.scanner.util;
+
+import java.util.List;
+
+public class OutputUtil {
+    public static String listToString(List<String> list, String separator) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            if (i < list.size() - 1) {
+                sb.append(separator);
+            }
+        }
+        return sb.toString();
+    }
+}
