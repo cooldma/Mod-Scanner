0d7d323bad57d7faafb7fc0d10a9783e9d3761be
sixnoinegames <100712573+sixnoinegames@users.noreply.github.com>
2023-08-17 19:15:22 -0400

    output coloring

--- src/main/java/net/scanner/Main.java
+++ src/main/java/net/scanner/Main.java
@@ -1,6 +1,8 @@
 package net.scanner;
 
-import net.scanner.util.*;
+import net.scanner.util.DoomsdayDetector;
+import net.scanner.util.FileUtil;
+import net.scanner.util.RecycleBin;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -14,24 +16,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static net.scanner.util.OutputUtil.listToString;
-import static net.scanner.util.OutputUtil.scanFile;
+import static net.scanner.util.OutputUtil.*;
 
 public class Main {
     public static List<String> output = new ArrayList<>();
     public static final String UserFolder = System.getProperty("user.home");
     public static String mcDir = UserFolder + "\\AppData\\Roaming\\.minecraft";
+    public static final String Default = "\u001B[0m";
+    public static final String Green = "\u001B[32m";
+    public static final String Red = "\u001B[31m";
+    public static final String White = "\u001B[97m";
+    public static final String Cyan = "\u001B[96m";
+    public static final String Pink = "\u001B[95m";
+    public static final String Gray = "\u001B[37m";
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         getMC();
-        System.out.println();
-        System.out.println("Doomsday: " + DoomsdayDetector.recentDoomsday() + ", ! This is NOT 100% accurate !");
-        System.out.println("Recently Modified Recycle Bin: " + RecycleBin.isModified());
 
-        scanFile(Path.of(mcDir + "\\mods"));
+        System.out.println(Green + "Directory being scanned: " + mcDir + "\n" + White);
+
+        System.out.println(Pink + "DOOMSDAY DETECTIONS are NOT 100% accurate" + White);
+        System.out.println("Doomsday: " + ColorBool(DoomsdayDetector.recentDoomsday()) + White);
+        System.out.println("Recently Modified Recycle Bin: " + ColorBool(RecycleBin.isModified()) + White);
+
+        Path scanDir = Path.of(mcDir + "\\mods");
+        for (File file : scanDir.toFile().listFiles()) {
+            scanFile(file);
+        }
 
         System.out.println(listToString(Main.output, "\n"));
 
-        System.out.println("\n! Use RECAF to decompile any flagged mods !\n");
+        System.out.println(Pink + "\n! Use RECAF to decompile any flagged mods !\n" + White);
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Press enter to exit..");
